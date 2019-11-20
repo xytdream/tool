8ba74a8b7f619e5dfd02511a8b28714d791c3348
xyt <xyt_dream@163.com>
2019-11-21 02:19:42 +0800

    2

--- tool.js
+++ tool.js
@@ -60,3 +60,60 @@ function move(obj , attr , target , speed , callback){
         }
     },30)   
 }
+
+
+// 该函数用于向一个元素中添加指定的class属性
+/*
+*参数：
+*   obj: 要添加class属性的元素对象
+*   cName: 要添加的class值
+*/ 
+function addClass(obj,cName){
+    //判断元素是否已有指定的class属性值，没有则添加，有则不变
+    if(!hasClass(obj,cName)){
+        obj.className += " " + cName;
+    }   
+}
+
+// 该函数用于判断一个元素中是否含有指定的class属性值
+/*
+*参数：
+*   obj: 要判断class属性的元素对象
+*   cName: 要判断的class属性值
+*/ 
+function hasClass(obj,cName){
+    //创建一个正则表达式
+    var reg = new RegExp("\\b"+cName+"\\b")
+
+    return reg.test(obj.className)
+}
+
+// 该函数用于移除元素中指定的class属性值
+/*
+*参数：
+*   obj: 要移除class属性的元素对象
+*   cName: 要移除的class属性值
+*/ 
+function removeClass(obj,cName){
+    //创建一个正则表达式
+    var reg = new RegExp("\\b"+cName+"\\b")
+    //将指定的属性值替换成空串即可达到移除效果
+    obj.className = obj.className.replace(reg , "")
+}
+
+// 该函数用于切换元素中指定的class属性值，有则移除，无则添加
+/*
+*参数：
+*   obj: 要切换class属性的元素对象
+*   cName: 要切换的class属性值
+*/ 
+function toggleClass(obj,cName){
+    //判断是否有该class
+    if(hasClass(obj,cName)){
+        //有，移除
+        removeClass(obj,cName)
+    }else{
+        //无，添加
+        addClass(obj,cName)
+    }
+}
